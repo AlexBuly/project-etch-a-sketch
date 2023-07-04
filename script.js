c20b080b1237cc3a63f91d77baf913f56e883ec6
Alex Buly <asbuly@icloud.com>
2023-07-04 14:26:40 -0400

    Add other rows

--- script.js
+++ script.js
@@ -22,21 +22,14 @@ squares the user wants.
 const container = document.querySelector(".container");
 
 // FOR i = gridSquares; i is less than or equal to 16; i++; 16 times
-
-for (let i = 0; i < 16; ++i) {
-    const squareDivs = document.createElement("div");
-    squareDivs.classList.add("squareDivs");
-    squareDivs.style.cssText = "height: 31.5px; width: 31.5px; border: 3px solid black;";
-    container.appendChild(squareDivs);
+for (let l = 0; l < 16; ++l) {
+    for (let i = 0; i < 16; ++i) {
+        const squareDivs = document.createElement("div");
+        squareDivs.classList.add("squareDivs");
+        squareDivs.style.cssText = "height: 31.5px; width: 31.5px; border: 3px solid black;";
+        container.appendChild(squareDivs);
+    }
 }
-
-for (let i = 0; i < 16; ++i) {
-    const squareDivs = document.createElement("div");
-    squareDivs.classList.add("squareDivs");
-    squareDivs.style.cssText = "height: 31.5px; width: 31.5px; border: 3px solid black;";
-    container.appendChild(squareDivs);
-}
-
 // flex-direction: column;
 // gridSquares hover fades in and out 
 // add hover class 
