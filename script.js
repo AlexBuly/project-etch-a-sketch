d35da00b7e545e41c7645ffd350f34b19a39f930
Alex Buly <asbuly@icloud.com>
2023-07-18 19:51:35 -0400

    Add else statement

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-/* The a grid container contains individual grid squares for 16x16 grid. This would be too cumbersome 
-to create in the HTML file so it needs to be done using a loop in JavaScript. 
-
-for (let i = gridSquares; i <= 256; i++);
-gridContainer.textContent = gridSquares;
-gridSquares.style.display: "grid;"
-gridSquares.style.cssText = "grid-template-columns: auto * 16; grid-template-rows: auto * 16;"
-
-for (let i = gridSquares; i <= 16; i++ * 16);
-display: flex; 
-flex-direction: column 
-
-gridContainer.textContent = gridSquares[i] + "<br>";
-
-When mouse hover over each element, the color changes. An eraser removes the color and reverts it back
-to white.
-
-A button is used to change the size of the grid squares. A loop is applied to shrink/enlarge the 
-squares by so many pixels and add/remove the appropriate number of squares to match the number of 
-squares the user wants. 
-*/
 const container = document.querySelector(".container");
+
 const sixteenGrid = () => {
   // FOR i = gridSquares; i is less than or equal to 16; i++; 16 times
   for (let i = 0; i < 256; ++i) {
@@ -67,6 +47,7 @@ const sixteenGrid = () => {
       item.remove();
       container.textContent = '';
   });
+    // prompt("Number of squares")
     let size = Number(prompt("Enter a number."));
     let n = 0;
     if (size <= 100) {
@@ -77,18 +58,11 @@ const sixteenGrid = () => {
         container.appendChild(gridSquares);
         gridSquares.style.cssText = `background-color: white; height: calc(100% / ${size}); width: calc(100% / ${size}); display: inline-flex; box-sizing: border-box; border: 1px solid black;`;
         }
-      } 
-    else if (size > 100) {
-        alert("Number too large.");
-        sixteenGrid();
+      } else if (size > 100) {
+          alert("Number is too large.");
+          sixteenGrid();
+      } else {
+          alert("Not a number.");
+          sixteenGrid();
       }
   });
-
-  
-
-// prompt("Number of squares")
-// IF number > 100 
-//alert("Number is too large")
-// grow by X && shrink by X 
-// add appropriate numbers of squares 
-
