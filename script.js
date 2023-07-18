fd393f1ff0408bd2fe21a209267478ac345cd854
Alex Buly <asbuly@icloud.com>
2023-07-18 13:42:12 -0400

    Border box sizing for squares and 100 is max input number

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ for (let i = 0; i < 256; ++i) {
   let squareDivs = document.createElement("div");
   squareDivs.classList.add("squareDivs");
   container.appendChild(squareDivs);
-  squareDivs.style.cssText = "background-color: white; height: calc(100% / 16); width: calc(100% / 16); display: inline-flex; margin-bottom: -4px; flex-wrap: wrap;"
+  squareDivs.style.cssText = "background-color: white; height: calc(100% / 16); width: calc(100% / 16); display: inline-flex; margin-bottom: -4px; flex-wrap: wrap; border: 1px solid black; box-sizing: border-box"
 }
   container.addEventListener("mouseover", (event) => {
     let target = event.target
@@ -65,16 +65,17 @@ for (let i = 0; i < 256; ++i) {
     });
     let size = Number(prompt("Enter a number."));
     let n = 0;
-    while (n < size * size) {
-      ++n;
-      var gridSquares = document.createElement("div");
-      gridSquares.classList.add("squareDivs");
-      container.appendChild(gridSquares);
-      gridSquares.style.cssText = `background-color: white; height: calc(100% / ${size}); width: calc(100% / ${size}); display: inline-flex; margin-bottom: -4px;`
+    if (size < 100) {
+      while (n < size * size) {
+        ++n;
+        var gridSquares = document.createElement("div");
+        gridSquares.classList.add("squareDivs");
+        container.appendChild(gridSquares);
+        gridSquares.style.cssText = `background-color: white; height: calc(100% / ${size}); width: calc(100% / ${size}); display: inline-flex; margin-bottom: -4px; box-sizing: border-box; border: 1px solid black;`
+        }
       } 
-      if (size > 100) {
+    if (size > 100) {
         alert("Number too large.");
-        gridSquares.remove()
       }
   });
 
