76a18b37025fcecd8e16ab08992b0d99f0da44b5
Alex Buly <asbuly@icloud.com>
2023-07-18 13:52:04 -0400

    Creating a functiofor default grid

--- script.js
+++ script.js
@@ -20,42 +20,45 @@ squares by so many pixels and add/remove the appropriate number of squares to ma
 squares the user wants. 
 */
 const container = document.querySelector(".container");
-// FOR i = gridSquares; i is less than or equal to 16; i++; 16 times
-for (let i = 0; i < 256; ++i) {
-  let squareDivs = document.createElement("div");
-  squareDivs.classList.add("squareDivs");
-  container.appendChild(squareDivs);
-  squareDivs.style.cssText = "background-color: white; height: calc(100% / 16); width: calc(100% / 16); display: inline-flex; margin-bottom: -4px; flex-wrap: wrap; border: 1px solid black; box-sizing: border-box"
-}
-  container.addEventListener("mouseover", (event) => {
-    let target = event.target
-    target.style.backgroundColor = "black";
-  });
-  // add click event listener for buttons
-  let eraser = document.querySelector(".erase");
-  eraser.addEventListener("click", () => {
+const sixteenGrid = () => {
+  // FOR i = gridSquares; i is less than or equal to 16; i++; 16 times
+  for (let i = 0; i < 256; ++i) {
+    let squareDivs = document.createElement("div");
+    squareDivs.classList.add("squareDivs");
+    container.appendChild(squareDivs);
+    squareDivs.style.cssText = "background-color: white; height: calc(100% / 16); width: calc(100% / 16); display: inline-flex; margin-bottom: -4px; flex-wrap: wrap; border: 1px solid black; box-sizing: border-box"
+  }
     container.addEventListener("mouseover", (event) => {
-      var target = event.target;
-      target.style.backgroundColor = "white";
-      container.style.backgroundColor = "black";
-    });
-  });
-
-  let reset = document.querySelector(".eraseAll");
-  reset.addEventListener("click", () => {
-    let squares = document.querySelectorAll(".squareDivs");
-    squares.forEach((item) => {
-      item.style.backgroundColor = "white";
-   });
-  });
-
-  let draw = document.querySelector(".pen");
-  draw.addEventListener("click", () => {
-    container.addEventListener("mouseover", (event) => {
-      var target = event.target;
+      let target = event.target
       target.style.backgroundColor = "black";
     });
-  });
+    // add click event listener for buttons
+    let eraser = document.querySelector(".erase");
+    eraser.addEventListener("click", () => {
+      container.addEventListener("mouseover", (event) => {
+        var target = event.target;
+        target.style.backgroundColor = "white";
+        container.style.backgroundColor = "black";
+      });
+    });
+
+    let reset = document.querySelector(".eraseAll");
+    reset.addEventListener("click", () => {
+      let squares = document.querySelectorAll(".squareDivs");
+      squares.forEach((item) => {
+        item.style.backgroundColor = "white";
+    });
+    });
+
+    let draw = document.querySelector(".pen");
+    draw.addEventListener("click", () => {
+      container.addEventListener("mouseover", (event) => {
+        var target = event.target;
+        target.style.backgroundColor = "black";
+      });
+    });
+  }
+  sixteenGrid();
 
   let adjustSize = document.querySelector(".adjustSize");
   let squares = document.querySelectorAll(".squareDivs");
@@ -76,6 +79,7 @@ for (let i = 0; i < 256; ++i) {
       } 
     if (size > 100) {
         alert("Number too large.");
+        sixteenGrid();
       }
   });
 
